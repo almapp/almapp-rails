d7a1d376b8449502314faee341ae8cdd2290f7ee
Patricio Lopez <pelopez2@uc.cl>
2014-12-13 10:18:40 -0300

    fixed invalid subdomain redirection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,27 +3,38 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :dump_to_root
   layout :get_layout
+
+  def dump_to_root
+    redirect_to root_url(subdomain: '') unless request.subdomain == '' || valid_subdomain?(request.subdomain)
+  end
 
   def current_organization
     current_organization ||= Organization.find_with_subdomain(request.subdomain)
   end
 
   def current_subdomain
-    get_subdomain(current_organization)
+    current_subdomain ||= request.subdomain
   end
 
-  def get_subdomain(organization)
-    organization.slug
+  def get_subdomain_for_organization(organization)
+    organization.slug.downcase
   end
 
-  helper_method :current_organization, :get_subdomain, :current_subdomain
+  def valid_subdomain?(subdomain)
+    SUBDOMAINS.include? subdomain.downcase
+  end
+
+  helper_method :current_organization, :get_subdomain_for_organization, :current_subdomain
 
   private
+    SUBDOMAINS = Organization.pluck(:slug).map(&:downcase)
+
     def get_layout
       if current_organization.present?
-        uri = current_organization.slug + '_layout'
-        uri.to_s.downcase
+        resource = current_organization.slug + '_layout'
+        resource.to_s.downcase
       else
         'application'
       end
