9af2db978d2ad67f76fd988e387865f083ddd721
Patricio Lopez <pelopez2@uc.cl>
2014-12-18 11:49:40 -0300

    teacher_test file was mssing

---
+++ test/models/teacher_test.rb
@@ -0,0 +1,23 @@
+# == Schema Information
+#
+# Table name: teachers
+#
+#  id                :integer          not null, primary key
+#  name              :string(255)
+#  email             :string(255)
+#  information       :text
+#  url               :string(255)
+#  created_at        :datetime
+#  updated_at        :datetime
+#  slug              :string(255)
+#  username          :string(255)      not null
+#  academic_unity_id :integer
+#
+
+require 'test_helper'
+
+class TeacherTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
