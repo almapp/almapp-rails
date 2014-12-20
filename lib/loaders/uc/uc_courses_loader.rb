959276c46ac3d75dbee6b8f1bf5c5e43ab3afe74
Patricio Lopez <pelopez2@uc.cl>
2014-12-19 22:38:05 -0300

    name changes and remove useless vars

--- lib/loaders/uc/uc_courses_loader.rb
+++ lib/loaders/uc/uc_courses_loader.rb
@@ -31,7 +31,7 @@ class UCCoursesLoader < CoursesLoader
     end
   end
 
-  def f(short_name)
+  def get_unity(short_name)
     AcademicUnity.find_by_short_name(short_name)
   end
 
@@ -39,13 +39,13 @@ class UCCoursesLoader < CoursesLoader
     "http://admisionyregistros.uc.cl/dara/libcursos/periodo2#{period}/ua#{unity}_#{page}.html"
   end
 
-  def remove_shit_chars(input)
+  def remove_shitty_chars(input)
     c = input[0]
     l = input[-1]
     if  c == ' ' || c == '-' || c == '('
-      remove_shit_chars(input[1..input.length-1])
+      remove_shitty_chars(input[1..input.length-1])
     elsif l == ')'
-      remove_shit_chars(input[0..input.length-2])
+      remove_shitty_chars(input[0..input.length-2])
     else
       input
     end
@@ -55,40 +55,40 @@ class UCCoursesLoader < CoursesLoader
     # {UNITY : Faculty}
     r = Hash.new
     #r[91] = f('Antropología') Same as Sociologia
-    r[11] = f('Agronomía e Ingeniería Forestal')
-    r[94] = f('Arquitectura')
-    r[33] = f('Arte')
-    r[2] = f('Astrofísica')
-    r[12] = f('Ciencias Biológicas')
-    r[16] = f('Ciencias de la Salud')
-    r[5] = f('Ciencias Económicas y Administrativas')
-    r[45] = f('Ciencia Política')
+    r[11] = get_unity('Agronomía e Ingeniería Forestal')
+    r[94] = get_unity('Arquitectura')
+    r[33] = get_unity('Arte')
+    r[2] = get_unity('Astrofísica')
+    r[12] = get_unity('Ciencias Biológicas')
+    r[16] = get_unity('Ciencias de la Salud')
+    r[5] = get_unity('Ciencias Económicas y Administrativas')
+    r[45] = get_unity('Ciencia Política')
     #r[9] = f('College') Diferent layout http://admisionyregistros.uc.cl/dara/libcursos/periodo21/ua94_3.html
-    r[28] = f('Comunicaciones')
-    r[1] = f('Construcción Civil')
-    r[17] = f('Derecho')
-    r[59] = f('Diseño')
-    r[20] = f('Educación')
-    r[13] = f('Enfermería')
-    r[51] = f('Estética')
-    r[95] = f('Estudios Urbanos y Territoriales')
-    r[67] = f('Filosofía')
-    r[3] = f('Física')
-    r[57] = f('Geografía')
-    r[56] = f('Historia')
-    r[4] = f('Ingeniería')
-    r[64] = f('Letras')
-    r[6] = f('Matemática')
-    r[14] = f('Medicina')
-    r[70] = f('Música')
-    r[15] = f('Odontología')
-    r[29] = f('Psicología')
-    r[10] = f('Química')
-    r[91] = f('Sociología')
-    r[34] = f('Teatro')
-    r[38] = f('Teología')
-    r[30] = f('Trabajo Social')
-    r[21] = f('Villarrica')
+    r[28] = get_unity('Comunicaciones')
+    r[1] = get_unity('Construcción Civil')
+    r[17] = get_unity('Derecho')
+    r[59] = get_unity('Diseño')
+    r[20] = get_unity('Educación')
+    r[13] = get_unity('Enfermería')
+    r[51] = get_unity('Estética')
+    r[95] = get_unity('Estudios Urbanos y Territoriales')
+    r[67] = get_unity('Filosofía')
+    r[3] = get_unity('Física')
+    r[57] = get_unity('Geografía')
+    r[56] = get_unity('Historia')
+    r[4] = get_unity('Ingeniería')
+    r[64] = get_unity('Letras')
+    r[6] = get_unity('Matemática')
+    r[14] = get_unity('Medicina')
+    r[70] = get_unity('Música')
+    r[15] = get_unity('Odontología')
+    r[29] = get_unity('Psicología')
+    r[10] = get_unity('Química')
+    r[91] = get_unity('Sociología')
+    r[34] = get_unity('Teatro')
+    r[38] = get_unity('Teología')
+    r[30] = get_unity('Trabajo Social')
+    r[21] = get_unity('Villarrica')
     return r
   end
 
@@ -114,7 +114,7 @@ class UCCoursesLoader < CoursesLoader
           puts 'Valid URL: '.concat(url)
           html = web.body
           doc = Nokogiri::HTML(html)
-          title = remove_shit_chars(doc.xpath('//body/table/tr/td/font/b/font').text)
+          title = remove_shitty_chars(doc.xpath('//body/table/tr/td/font/b/font').text)
 
           unity.name = title.titleize
           unity.save
@@ -132,8 +132,8 @@ class UCCoursesLoader < CoursesLoader
             if course.present?
               puts 'Course found: '.concat(course.initials)
             else
-              credits = remove_shit_chars(attributes[3].text)
-              name = remove_shit_chars(attributes[4].text)
+              credits = remove_shitty_chars(attributes[3].text)
+              name = remove_shitty_chars(attributes[4].text)
               course = Course.create(initials: initial,
                                      name: name,
                                      credits: credits,
@@ -141,7 +141,7 @@ class UCCoursesLoader < CoursesLoader
               puts 'Course not found, created: '.concat(course.inspect)
             end
 
-            section_number = remove_shit_chars(attributes[2].text)
+            section_number = remove_shitty_chars(attributes[2].text)
             section = Section.find_by_course_id_and_year_and_semester_and_number(course.id, year, semester, section_number)
             if section.present?
               puts 'Section found: '.concat(section.identifier)
@@ -160,7 +160,7 @@ class UCCoursesLoader < CoursesLoader
             teachers = attributes[8].xpath('font')
             teachers.children.each do |t|
               if t.text?
-                t_name = remove_shit_chars(t.text)
+                t_name = remove_shitty_chars(t.text)
                 if t_name.downcase == 'sin profesores' || t_name.downcase == 'por definir'
                   puts course.name.concat(' has no teachers.')
                 else
@@ -182,13 +182,13 @@ class UCCoursesLoader < CoursesLoader
             schedules = attributes[9].xpath('font').children
             module_types = attributes[10].xpath('font').children
             classrooms = attributes[11].xpath('font').children
-            campus = remove_shit_chars(attributes[12].xpath('font').children.first.text) rescue ''
+            campus = remove_shitty_chars(attributes[12].xpath('font').children.first.text) rescue ''
 
             for i in 0..module_types.length-1 do
               if module_types[i].text?
-                module_type = remove_shit_chars(module_types[i].text) rescue ''
-                module_block = remove_shit_chars(schedules[i].text) rescue ''
-                module_place = remove_shit_chars(classrooms[i].text) rescue ''
+                module_type = remove_shitty_chars(module_types[i].text) rescue ''
+                module_block = remove_shitty_chars(schedules[i].text) rescue ''
+                module_place = remove_shitty_chars(classrooms[i].text) rescue ''
 
                 puts '-------------'
                 puts 'Vars:'
@@ -199,11 +199,9 @@ class UCCoursesLoader < CoursesLoader
                 puts 'Campus: '.concat(campus)
                 puts 'Place: '.concat(module_place)
 
-                alt_place = ''
                 c_array = module_place.split(': ')
                 if c_array.size == 2
-                  module_place = remove_shit_chars(c_array[1])
-                  alt_place = remove_shit_chars(c_array[0])
+                  module_place = remove_shitty_chars(c_array[1])
                 end
 
                 puts 'Final Place: '.concat(module_place)
